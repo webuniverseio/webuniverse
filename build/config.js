6b49ae89cf12b23c777c47f48b9c2c594e4e58b5
Sergey Zarouski <serge.zarouski@gmail.com>
2015-04-03 13:05:21 -0400

    updates

---
+++ build/config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var path = require('path');
+
+//noinspection SpellCheckingInspection
+module.exports = [{
+	link: 'https://dl.bintray.com/oneclick/rubyinstaller/rubyinstaller-2.2.1-x64.exe?direct',
+	name: 'ruby',
+	postfix: ' /verysilent /dir="c:\\Ruby2" /tasks="assocfiles,modpath"'
+},
+{
+	link: 'http://github.com/msysgit/msysgit/releases/download/' +
+	'Git-1.9.4-preview20140929/Git-1.9.4-preview20140929.exe',
+	name: 'git.exe',
+	postfix: ' /DIR="c:\\Git" /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /NOICONS ' +
+	'/COMPONENTS="icons,ext\\reg\\shellhere,assoc,assoc_sh" /LOADINF="' + path.resolve(__dirname, 'git.inf') + '"'
+}, {
+	prefix: 'npm i ',
+	name: 'karma',
+	postfix: '-cli -g'
+}, {
+	prefix: 'npm i ',
+	name: 'grunt',
+	postfix: '-cli -g'
+}, {
+	prefix: 'npm i ',
+	name: 'bower',
+	postfix: ' -g'
+}, {
+	prefix: 'SET PATH=%PATH%;c:\\Ruby2\\bin&&gem install ',
+	name: 'compass',
+	postfix: ' -v 1.0.1'
+}];
