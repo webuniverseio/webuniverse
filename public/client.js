3956a8e767f089bf97995c39e5eba2c047599fde
Glitch (hello-express) <none>
2019-01-04 21:02:54 +0000

    👮🙄 Checkpoint ./views/index.html:111865/1090 ./server.js:111865/327 ./public/client.js:111865/2555

--- public/client.js
+++ public/client.js
@@ -1,42 +1,20 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-console.log('hello world :o');
-
-// our default array of dreams
-const dreams = [
-  'Find and count some sheep',
-  'Climb a really tall mountain',
-  'Wash the dishes'
-];
-
-// define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
-
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
-}
-
-// iterate through every dream and add it to our page
-dreams.forEach( function(dream) {
-  appendNewDream(dream);
+document.addEventListener('keydown', (e) => {
+  let text = e.type +
+    ' key=' + e.key +
+    ' code=' + e.code +
+    (e.shiftKey ? ' shiftKey' : '') +
+    (e.ctrlKey ? ' ctrlKey' : '') +
+    (e.altKey ? ' altKey' : '') +
+    (e.metaKey ? ' metaKey' : '') +
+    (e.repeat ? ' (repeat)' : '') +
+    "\n";
+  console.log(text);
+}, true);
+document.addEventListener('focus', (e) => {
+  console.log('focused');
 });
-
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
-
-  // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
-};
+setTimeout(() => {
+  document.body.insertAdjacentHTML('beforeend', `<a href="https://homedepot.ca?secrets=gone" id="totally-unique">I'm just a friendly link</a>`);
+  console.log('added iframe');
+  document.querySelector('#totally-unique').click();
+}, 5000)
--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  res.set('Content-Security-Policy', `default-src 'self'`);
+  next();
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
