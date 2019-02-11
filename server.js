9f929a9fef98942d6cbf0f3a14358db0dd77c0a4
Sergey Zarouski <serge.zarouski@gmail.com>
2019-02-10 22:05:36 -0500

    set cache-control

--- server.js
+++ server.js
@@ -4,15 +4,22 @@
 // init project
 var express = require('express');
 var app = express();
-var path = require('path');
+var url = require('url');
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
+app.use(function setCacheControl(req, res, next) {
+  if (/\.(css|js|jpg|png|gif)$/.test(url.parse(req.url).pathname)) {
+    res.setHeader('Cache-Control', 'public, max-age=31536000');
+  }
+  next();
+});
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-app.use(function (req, res, next) {
+app.use(function handle404(req, res, next) {
   res.status(404).sendFile(__dirname + '/public/404.html');;
 });
 
