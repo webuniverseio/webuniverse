387ba8326e8a09752f40dcd7d89d2150f41e252d
Glitch (webuniverse) <none>
2019-02-11 03:04:06 +0000

    💃📅 Checkpoint ./server.js:111865/328

--- server.js
+++ server.js
@@ -4,21 +4,16 @@
 // init project
 var express = require('express');
 var app = express();
+var path = require('path');
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-app.use((req, res, next) => {
-  console.log('yeah');
-  next();
-});
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-app.use((err, req, res, next) => {
-  console.error({err});
-  next();
+app.use(function (req, res, next) {
+  res.status(404).sendFile(__dirname + '/public/404.html');;
 });
 
 // listen for requests :)
